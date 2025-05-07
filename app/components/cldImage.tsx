e448d93e81cc4910992827bc9f9803c3455ca6b6
Takenori <takenori113@gmail.com>
2025-05-07 23:29:33 +0900

    add size

--- app/components/cldImage.tsx
+++ app/components/cldImage.tsx
@@ -10,6 +10,8 @@ export default function CImage({ src }: Props) {
   return (
     <CldImage
       alt="Sample Image"
+      width={500}
+      height={500}
       src={src} // Use this sample image or upload your own via the Media Explorer
       crop={{
         type: "auto",
